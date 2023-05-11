f9a72fafa23b52e6f1ee19e8d8e749c6950fb058
SHIJU G <85386417+shiju-g@users.noreply.github.com>
2023-05-11 11:13:46 +0530

    new update

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
 import BannerSlider from "../components/BannerSlider";
 import ContactCard from "@/components/ContactCard";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <div className="relative ">
-      <div className="absolute w-full top-0 pt-32">
-        <BannerSlider />
-        <ContactCard />
+    <Suspense fallback={<p>Loading....</p>}>
+      <div className="relative ">
+        <div className="absolute w-full top-0 pt-32">
+          <BannerSlider />
+          <ContactCard />
+        </div>
+
+        <img
+          src="/img/food-background.png"
+          className="w-full h-screen object-cover"
+          alt="dfsa"
+        />
       </div>
-      <img
-        src="/img/food-background.png"
-        className="w-full h-screen object-cover"
-        alt="dfsa"
-      />
-    </div>
+    </Suspense>
   );
 }
--- components/BannerSlider.tsx
+++ components/BannerSlider.tsx
@@ -1,12 +1,30 @@
+"use client";
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+
+import { Autoplay } from "swiper";
 
 const BannerSlider = () => {
   return (
-    <div className="xl:w-7/12 lg:w-9/12 md:w-10/12 w-11/12 mx-auto p-5 mt-5 bg-[#0346090D] text-center font-bold text-2xl tracking-wider rounded-full shadow-lg">
-      <h1 className="text-[#143e05] ">
-        "Bringing the taste of celebration to your special event"
-      </h1>
-    </div>
+    <Swiper
+      autoplay={{ delay: 1500 }}
+      speed={1500}
+      loop={true}
+      modules={[Autoplay]}
+      className="mySwiper xl:w-7/12 lg:w-9/12 md:w-10/12 relative w-11/12 mx-auto  mt-5 bg-[#0346090D] text-center font-bold text-2xl tracking-wider rounded-full shadow-lg"
+    >
+      <SwiperSlide className="text-[#143e05] py-5">
+        <h1>"Bringing the taste of celebration to your special event"</h1>
+      </SwiperSlide>
+      <SwiperSlide className="text-[#143e05] py-5">
+        <h1>"Bringing the taste of celebration to your special event"</h1>
+      </SwiperSlide>
+      <SwiperSlide className="text-[#143e05] py-5">
+        <h1>"Bringing the taste of celebration to your special event"</h1>
+      </SwiperSlide>
+    </Swiper>
   );
 };
 
