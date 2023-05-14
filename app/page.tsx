eebc21ad8c866168b7682c73402edc9f516fd7ad
SHIJU G <85386417+shiju-g@users.noreply.github.com>
2023-05-14 10:10:04 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,15 @@ import { Suspense } from "react";
 export default function Home() {
   return (
     <Suspense fallback={<p>Loading....</p>}>
-      <div className="relative ">
-        <div className="absolute w-full top-0 pt-32">
+      <div className="relative overflow-hidden">
+        <div className="absolute w-full top-0 sm:pt-36 pt-28">
           <BannerSlider />
           <ContactCard />
         </div>
 
         <img
           src="/img/food-background.png"
-          className="w-full h-screen object-cover"
+          className="w-full h-screen object-cover sm:pt-0 pt-32"
           alt="dfsa"
         />
       </div>
