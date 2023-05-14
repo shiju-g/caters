df503332ec75d2395d243e64b8c3bf2a5a83bc9a
SHIJU G <85386417+shiju-g@users.noreply.github.com>
2023-05-14 10:10:13 +0530

    Update BannerSlider.tsx

--- components/BannerSlider.tsx
+++ components/BannerSlider.tsx
@@ -6,6 +6,33 @@ import "swiper/css/navigation";
 
 import { Autoplay } from "swiper";
 
+const sliderArray = [
+  {
+    id: 1,
+    qoutes: "Bringing the taste of celebration to your special event",
+  },
+  {
+    id: 2,
+    qoutes: "Savor the flavor: Experience our exquisite catering services",
+  },
+  {
+    id: 3,
+    qoutes: "Elevate your event with our gourmet catering options",
+  },
+  {
+    id: 4,
+    qoutes: "Impress your guests with our mouth-watering catering menu",
+  },
+  {
+    id: 5,
+    qoutes: "Indulge in our delicious catering creations",
+  },
+  {
+    id: 6,
+    qoutes: "Catering that delights the senses and satisfies the palate",
+  },
+];
+
 const BannerSlider = () => {
   return (
     <Swiper
@@ -13,17 +40,14 @@ const BannerSlider = () => {
       speed={1500}
       loop={true}
       modules={[Autoplay]}
-      className="mySwiper xl:w-7/12 lg:w-9/12 md:w-10/12 relative w-11/12 mx-auto  mt-5 bg-[#0346090D] text-center font-bold text-2xl tracking-wider rounded-full shadow-lg"
+      className="mySwiper xl:w-7/12 lg:w-9/12 md:w-10/12 relative w-11/12  mx-auto  md:mt-5 bg-[#0346090D] text-center font-bold xl:text-2xl sm:text-xl text-sm tracking-wider md:rounded-full rounded-3xl px-5 shadow-lg"
     >
-      <SwiperSlide className="text-[#143e05] py-5">
-        <h1>"Bringing the taste of celebration to your special event"</h1>
-      </SwiperSlide>
-      <SwiperSlide className="text-[#143e05] py-5">
-        <h1>"Bringing the taste of celebration to your special event"</h1>
-      </SwiperSlide>
-      <SwiperSlide className="text-[#143e05] py-5">
-        <h1>"Bringing the taste of celebration to your special event"</h1>
-      </SwiperSlide>
+      {sliderArray &&
+        sliderArray.map((list) => (
+          <SwiperSlide key={list.id} className="text-[#143e05] py-5">
+            <h1>{`${`"`}${list.qoutes}${`"`}`}</h1>
+          </SwiperSlide>
+        ))}
     </Swiper>
   );
 };
