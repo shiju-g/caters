74614c2d0b3ee9267f7b44f3b890628bc20d3dcb
SHIJU G <85386417+shiju-g@users.noreply.github.com>
2023-05-14 13:11:39 +0530

    Update ContactCard.tsx

--- components/ContactCard.tsx
+++ components/ContactCard.tsx
@@ -5,16 +5,16 @@ type Props = {};
 const ContactCard = (props: Props) => {
   return (
     <div
-      className=" md:w-fit sm:w-10/12 w-11/12 md:px-20 px-5 rounded-2xl shadow tracking-wider mx-auto md:mt-10 mt-5 text-center  py-8"
+      className=" md:w-fit cursor-pointer sm:w-10/12 w-11/12 md:px-20 px-5 rounded-2xl shadow tracking-wider mx-auto md:mt-10 mt-5 text-center  py-8"
       style={{ background: "rgba(255, 165, 0, 0.1)" }}
     >
-      <h1 className="xl:text-3xl md:text-2xl text-xl font-extrabold ">
+      <h1 className="xl:text-3xl md:text-2xl text-xl font-extrabold hover:scale-105 duration-300">
         9388596889
       </h1>
-      <p className="md:text-base text-sm text-gray-600 font-semibold my-1">
+      <p className="md:text-base text-sm text-gray-600 font-semibold my-1 hover:scale-105 duration-300">
         srigiricatering@mail.com
       </p>
-      <p className="md:text-lg text-base md:mt-auto mt-1 text-[#FFA500] font-semibold">
+      <p className="md:text-lg text-base md:mt-auto mt-1 text-[#FFA500] font-semibold hover:scale-105 duration-200">
         Allinivas House, VP Thara , Kollengode, Palakkad
       </p>
     </div>
