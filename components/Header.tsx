4f873272f11b7271af65948c5c091dacc8df9679
SHIJU G <85386417+shiju-g@users.noreply.github.com>
2023-05-20 22:01:02 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 const Header = () => {
   return (
@@ -11,14 +14,27 @@ const Header = () => {
           "linear-gradient(0deg, rgba(0, 114, 11, 0.9), rgba(0, 114, 11, 0.7)), #FFFFFF;",
       }}
     >
-      <Image
-        className=" rounded-lg -ml-1.5"
-        src="/img/srigiri.png"
-        width={250}
-        height={80}
-        alt="logo"
-        priority
-      />
+      <motion.div
+        initial={{ x: -100, scale: 0.8, opacity: 0 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut",
+          times: [1],
+          repeat: Infinity,
+          repeatDelay: 15,
+        }}
+        className="w-fit"
+      >
+        <Image
+          className=" rounded-lg -ml-1.5"
+          src="/img/srigiri.png"
+          width={250}
+          height={80}
+          alt="logo"
+          priority
+        />
+      </motion.div>
     </div>
   );
 };
