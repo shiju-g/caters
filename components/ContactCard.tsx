382a6d30dd156b11a12be982c55a133a7ae3c036
SHIJU G <85386417+shiju-g@users.noreply.github.com>
2023-05-29 20:01:41 +0530

    Update ContactCard.tsx

--- components/ContactCard.tsx
+++ components/ContactCard.tsx
@@ -15,22 +15,24 @@ const ContactCard = (props: Props) => {
         stiffness: 100,
         damping: 20,
       }}
-      className=" md:w-fit cursor-pointer sm:w-10/12 w-11/12 md:px-20 px-5 rounded-2xl shadow tracking-wider mx-auto md:mt-10 mt-5 text-center  py-8"
+      className=" md:w-fit  sm:w-10/12 w-11/12 md:px-20 px-5 rounded-2xl shadow tracking-wider mx-auto md:mt-10 mt-5 text-center  py-8"
       style={{
         background:
           "linear-gradient(0deg, rgba(255, 199, 0, 0.17), rgba(255, 199, 0, 0.17)), #FFFFFF;",
       }}
     >
-      <Link
-        href="tell:9388596889"
-        className="xl:text-3xl md:text-2xl text-xl font-extrabold hover:scale-105 duration-300"
-      >
-        9388596889
+      <Link href="tel:+919388596889" target="_blank" legacyBehavior>
+        <a
+          className="xl:text-3xl md:text-2xl text-xl font-extrabold hover:scale-105 duration-300"
+          href="tel:+919388596889"
+        >
+          9388596889
+        </a>
       </Link>
-      <p className="md:text-base text-sm text-gray-600 font-semibold my-1 hover:scale-105 duration-300">
+      <p className="md:text-base text-sm text-gray-600 font-semibold my-1  duration-300">
         srigiricatering@gmail.com
       </p>
-      <p className="md:text-lg text-sm mt-2.5 leading-tight   md:mt-auto  text-[#FFA500] font-semibold hover:scale-105 duration-200">
+      <p className="md:text-lg text-sm mt-2.5 leading-tight   md:mt-auto  text-[#FFA500] font-semibold duration-200">
         Allinivas House, VP Thara , Kollengode, Palakkad
       </p>
     </motion.div>
